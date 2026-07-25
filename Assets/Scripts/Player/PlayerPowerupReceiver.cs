753388787a6f304afebfd426d0763ca1c8ecaae6
packet-nova <90776963+packet-nova@users.noreply.github.com>
2026-07-25 11:36:00 -0400

    Player powerup receiver fixed and updated

--- Assets/Scripts/Player/PlayerPowerupReceiver.cs
+++ Assets/Scripts/Player/PlayerPowerupReceiver.cs
@@ -1,9 +1,17 @@
-﻿namespace ExploderGuy
+﻿using UnityEngine;
+
+namespace ExploderGuy
 {
-    public class PlayerPowerUpReceiver
+    public class PlayerPowerUpReceiver : MonoBehaviour
     {
         private PlayerBombPlacer _bombPlacer;
         private PlayerState _playerState;
+
+        private void Awake()
+        {
+            _bombPlacer = GetComponent<PlayerBombPlacer>();
+            _playerState = GetComponent<PlayerState>();
+        }
 
         public void AddMoveSpeedLevel()
         {
